14aa5bf6944d88c6ea4679fa90b35eb147eb8a0d
Benjamin Petrin <bpetrin@dyn.com>
2014-05-05 11:30:57 -0400

    Initial commit

---
+++ GraniteStateDogRecovery/Granite State Dog Recovery/GSDRMasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  GSDRMasterViewController.h
+//  Granite State Dog Recovery
+//
+//  Created by Benjamin Petrin on 5/5/14.
+//  Copyright (c) 2014 Granite State Dog Recovery. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GSDRMasterViewController : UITableViewController
+
+@end
