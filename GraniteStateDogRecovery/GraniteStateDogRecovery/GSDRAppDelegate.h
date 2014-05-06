bb6883b7de46a9f4db33db47f413766c196cf870
Christine Graham <cgraham@cgraham.local>
2014-05-06 01:12:35 -0400

    Updated project structure to adhere to new naming conventions (no spaces). Created basic layout with placeholder buttons. The storyboard now includes the 3 main sections as view controllers, the segues to them, and the navigation and main view controllers. Each of the 3 main sections will need their own set of sub views using the same navigation stack as the app itself. New .h/.m files will be needed for each of the view controllers.

---
+++ GraniteStateDogRecovery/GraniteStateDogRecovery/GSDRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GSDRAppDelegate.h
+//  Granite State Dog Recovery
+//
+//  Created by Benjamin Petrin on 5/5/14.
+//  Copyright (c) 2014 Granite State Dog Recovery. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GSDRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GraniteStateDogRecovery/GraniteStateDogRecovery/GSDRMainViewController.m
@@ -0,0 +1,49 @@
+//
+//  GSDRMainViewController.m
+//  GraniteStateDogRecovery
+//
+//  Created by Christine Graham on 5/6/14.
+//  Copyright (c) 2014 Granite State Dog Recovery. All rights reserved.
+//
+
+#import "GSDRMainViewController.h"
+
+@interface GSDRMainViewController ()
+
+@end
+
+@implementation GSDRMainViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
